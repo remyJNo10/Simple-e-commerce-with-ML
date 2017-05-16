77487d781d47ccc0c3d941d86f64aff253a94f0f
Jeremy Jackson <jj.cseng@gmail.com>
2017-05-16 15:39:32 +0530

    final

--- login/views.py
+++ login/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+
+import re
 
 
 # Create your views here.
@@ -11,9 +14,17 @@ def ShowLogin(request):
 def MakeRegistration(request):
     username = request.POST["username"]
     email = request.POST["email"]
+    EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if not EMAIL_REGEX.match(email):
+        return HttpResponse("Enter a valid email")
     password = request.POST["password"]
-    User.objects.create_user(username,email,password)
-    return redirect("/")
+    user = authenticate(username=username, password=password)
+    if user is None:
+        User.objects.create_user(username,email,password)
+        return redirect("/")
+    else:
+        return HttpResponse("Please try again")
+    
 
 def Login(request):
     username = request.POST["username"]
